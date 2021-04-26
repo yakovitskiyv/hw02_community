39b4cd09c24aee4fa3d450992c7fae3514363e52
Vladislav Yakovitskiy <yakovitskiyv@yandex.ru>
2021-04-26 14:49:27 +0300

    refactoring to meet PEP8 standart 6

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8o+y3guv1qa8u(ktsi#v)yo*67z(nwsf5xe8$whb4tx^^s3nad'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
