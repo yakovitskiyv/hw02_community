411d8ebbbd1ed646e943842a7da4ee92d1696ca5
Yakovitskiy Vladislav <yakovitskiyv@yandex.ru>
2021-04-26 12:23:08 +0300

    add header.html, reforming group.html

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -11,8 +11,16 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
 
+class GroupAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("slug",)
+    list_filter = ("slug",)
+    empty_value_display = "-пусто-" 
+    
+
 
 # при регистрации модели Post источником конфигураци
 # для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/apps.py
+++ yatube/posts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     name = 'posts'
     verbose_name = 'Управление публикациями posts'
+
+class GroupConfig(AppConfig):
+    name = 'group'
+    verbose_name = 'Управление сообществами'
